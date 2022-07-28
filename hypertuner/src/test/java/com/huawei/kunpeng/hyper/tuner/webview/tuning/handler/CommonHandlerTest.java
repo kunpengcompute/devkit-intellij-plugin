86835e1103d8c69b659361b54a6cc16082a22f40
纪锋 <feng.ji@epro.com.cn>
2022-07-28 16:45:13 +0800

    clean code

--- hypertuner/src/test/java/com/huawei/kunpeng/hyper/tuner/webview/tuning/handler/CommonHandlerTest.java
+++ hypertuner/src/test/java/com/huawei/kunpeng/hyper/tuner/webview/tuning/handler/CommonHandlerTest.java
@@ -1,21 +1,9 @@
 package com.huawei.kunpeng.hyper.tuner.webview.tuning.handler;
 
-import com.huawei.kunpeng.hyper.tuner.common.constant.TuningIDEConstant;
-import com.huawei.kunpeng.hyper.tuner.common.utils.TuningCommonUtil;
-import com.huawei.kunpeng.intellij.common.log.Logger;
-import com.huawei.kunpeng.intellij.common.util.CommonUtil;
-import com.huawei.kunpeng.intellij.common.util.FileUtil;
 import com.huawei.kunpeng.intellij.js2java.bean.MessageBean;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-
-import java.io.File;
-import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * webview cmd 函数类测试
