7e23c310b36b3b31ea4ae6b53056ea84cb6af415
figureair <1648438626@qq.com>
2022-12-10 07:01:25 +0800

    feat: 异常处理刷新按钮

---
+++ hypertuner/src/main/java/com/huawei/kunpeng/hyper/tuner/toolview/panel/impl/TuningConnectFailPanel.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2022. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huawei.kunpeng.hyper.tuner.toolview.panel.impl;
+
+
+import com.huawei.kunpeng.hyper.tuner.common.constant.enums.PanelType;
+import com.huawei.kunpeng.hyper.tuner.common.i18n.TuningI18NServer;
+import com.huawei.kunpeng.hyper.tuner.webview.tuning.pageeditor.ConfigureServerEditor;
+import com.huawei.kunpeng.hyper.tuner.webview.tuning.pageeditor.FreeTrialEditor;
+import com.huawei.kunpeng.intellij.common.constant.IDEConstant;
+import com.huawei.kunpeng.intellij.common.util.BaseIntellijIcons;
+import com.huawei.kunpeng.intellij.common.util.CommonUtil;
+import com.huawei.kunpeng.intellij.common.util.StringUtil;
+import com.huawei.kunpeng.intellij.ui.action.IDEPanelBaseAction;
+import com.huawei.kunpeng.intellij.ui.panel.IDEBasePanel;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Map;
+
+public class TuningConnectFailPanel extends IDEBasePanel {
+    // 滚动条面板
+    private JScrollPane scrollPanel;
+
+    private JPanel mainPanel;
+    private JLabel ipLabel;
+    private JLabel portLabel;
+    private JLabel ipInfoLabel;
+    private JLabel portInfoLabel;
+    private JLabel connectFailLabel;
+    private JButton configServerButton;
+    private JLabel freeTrialLabel;
+    private JButton freeTrialButton;
+    private JPanel contentPanel;
+    private JLabel statusLabel;
+    private JLabel statusInfoLabel;
+    private JLabel failLabel;
+    private JLabel failInfoLabel;
+
+    private Project project;
+
+    // 已配置服务器的ip和端口
+    private String ip;
+    private String port;
+
+    private String failInfo;
+
+    /**
+     * 左侧树登录面板构造函数
+     *
+     * @param toolWindow 工具窗口
+     * @param panelName  面板名称
+     * @param project    当前的项目
+     */
+    public TuningConnectFailPanel(ToolWindow toolWindow, String panelName, Project project, String failInfo) {
+        this.failInfo = failInfo;
+        this.project = project;
+        setToolWindow(toolWindow);
+        this.panelName = StringUtil.stringIsEmpty(panelName) ? PanelType.TUNING_CONNECT_FAIL.panelName() : panelName;
+        initPanel();
+        registerComponentAction();
+        createContent(mainPanel, null, false);
+    }
+
+    /**
+     * 反射调用的构造函数
+     *
+     * @param toolWindow 工具窗口
+     * @param project    当前的项目
+     */
+    public TuningConnectFailPanel(ToolWindow toolWindow, Project project, String failInfo) {
+        this(toolWindow, null, project, failInfo);
+    }
+
+    private void initPanel() {
+        // 取消滚动条面板的边框
+        scrollPanel.setBorder(null);
+        Map<String, String> serverConfig = CommonUtil.readCurIpAndPortFromConfig();
+        ip = serverConfig.get("ip");
+        port = serverConfig.get("port");
+        ipInfoLabel.setText(ip);
+        portInfoLabel.setText(port);
+        ipLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_ip_address"));
+        portLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_port"));
+        statusLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_status"));
+        statusInfoLabel.setIcon(BaseIntellijIcons.load(IDEConstant.RED_POINT_PATH));
+        statusInfoLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_fail_status"));
+        failLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_fail_info"));
+        failInfoLabel.setText(this.failInfo);
+        connectFailLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_connect_server_fail"));
+        configServerButton.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_server_config_now"));
+        configServerButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        freeTrialLabel.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_free_trial_text"));
+        freeTrialButton.setText(TuningI18NServer.toLocale("plugins_hyper_tuner_lefttree_free_trial_button"));
+        freeTrialButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    }
+
+    @Override
+    protected void registerComponentAction() {
+        MouseAdapter configMouseAdapter = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ConfigureServerEditor.openPage();
+            }
+        };
+        MouseAdapter freeTrialMouseAdapter = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                FreeTrialEditor.openPage();
+            }
+        };
+        configServerButton.addMouseListener(configMouseAdapter);
+        freeTrialButton.addMouseListener(freeTrialMouseAdapter);
+
+    }
+
+    @Override
+    protected void setAction(IDEPanelBaseAction action) {
+    }
+}
--- hypertuner/src/main/java/com/huawei/kunpeng/hyper/tuner/toolview/sourcetuning/RefreshConnectionAction.java
+++ hypertuner/src/main/java/com/huawei/kunpeng/hyper/tuner/toolview/sourcetuning/RefreshConnectionAction.java
@@ -1,28 +1,97 @@
 package com.huawei.kunpeng.hyper.tuner.toolview.sourcetuning;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.huawei.kunpeng.hyper.tuner.common.constant.TuningIDEConstant;
 import com.huawei.kunpeng.hyper.tuner.common.i18n.TuningI18NServer;
+import com.huawei.kunpeng.hyper.tuner.http.TuningHttpsServer;
+import com.huawei.kunpeng.hyper.tuner.toolview.dialog.impl.CompatibilityDialog;
+import com.huawei.kunpeng.hyper.tuner.toolview.panel.impl.TuningConfigSuccessPanel;
+import com.huawei.kunpeng.hyper.tuner.toolview.panel.impl.TuningConnectFailPanel;
+import com.huawei.kunpeng.intellij.common.bean.RequestDataBean;
+import com.huawei.kunpeng.intellij.common.bean.ResponseBean;
 import com.huawei.kunpeng.intellij.common.constant.IDEConstant;
+import com.huawei.kunpeng.intellij.common.enums.ConfigProperty;
+import com.huawei.kunpeng.intellij.common.enums.HttpMethod;
+import com.huawei.kunpeng.intellij.common.log.Logger;
 import com.huawei.kunpeng.intellij.common.util.BaseIntellijIcons;
 import com.huawei.kunpeng.intellij.common.util.CommonUtil;
+import com.huawei.kunpeng.intellij.common.util.FileUtil;
 import com.huawei.kunpeng.intellij.common.util.StringUtil;
+import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
 
 public class RefreshConnectionAction extends AnAction implements DumbAware {
     private static final Icon icon = BaseIntellijIcons.load(IDEConstant.MENU_ICONS_PATH + IDEConstant.TITLE_REFRESH_ICON);
 
     private static final String REFRESH_CONNECTION = TuningI18NServer.toLocale("plugins_hyper_tuner_titlebar_refresh_connection");
+
     public RefreshConnectionAction() {
         super(REFRESH_CONNECTION, null, icon);
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        // TODO 刷新连接按钮事件
+        String failInfo = null;
+
+        // 检查版本接口模拟
+
+        Callable task = new Callable() {
+            @Override
+            public ResponseBean call() throws Exception {
+                String SERVER_VERSION_URL = "/user-management/api/v2.2/users/version/";
+                RequestDataBean message = new RequestDataBean(TuningIDEConstant.TOOL_NAME_TUNING, SERVER_VERSION_URL,
+                        HttpMethod.GET.vaLue(), false);
+                ResponseBean responseBean = TuningHttpsServer.INSTANCE.requestData(message);
+                return responseBean;
+            }
+        };
+        ExecutorService exeservices = Executors.newSingleThreadExecutor();
+        Future future = exeservices.submit(task);
+        try {
+            //超时处理，否则界面会卡住很久
+            ResponseBean responseBean = (ResponseBean) future.get(2, TimeUnit.SECONDS);
+            if (responseBean == null) {
+                Logger.info("Server Version not found.");
+                failInfo = TuningI18NServer.toLocale("plugins_hyper_tuner_refresh_connect_fail");
+            } else {
+                Logger.info(responseBean.toString());
+                String responseBeanDataJsStr = responseBean.getData();
+                JSONObject jsonObject = JSON.parseObject(responseBeanDataJsStr);
+                String serverVersionStr = jsonObject.getString("version");
+                boolean isContains = true; // 默认插件兼容所有版本插件
+                Map config = FileUtil.ConfigParser.parseJsonConfigFromFile(IDEConstant.CONFIG_PATH);
+                Object configVersionObj = config.get(ConfigProperty.CONFIG_VERSION.vaLue());
+                if (configVersionObj instanceof List) {
+                    List configList = (List) configVersionObj;
+                    if (!configList.isEmpty()) {
+                        // 配置文件中兼容性版本不为空，则说明对兼容性有要求
+                        isContains = configList.contains(serverVersionStr);
+                    }
+                }
+                if (!isContains) {
+                    failInfo = TuningI18NServer.toLocale("plugins_hyper_tuner_refresh_version_not_match");
+                }
+            }
+            if (failInfo != null) synchronizedLeftTree(failInfo);
+        } catch (Exception error) {
+            Logger.info(error.getMessage());
+            failInfo = TuningI18NServer.toLocale("plugins_hyper_tuner_refresh_connect_fail");
+            synchronizedLeftTree(failInfo);
+        }
     }
 
     @Override
@@ -33,5 +102,27 @@ public class RefreshConnectionAction extends AnAction implements DumbAware {
         } else {
             event.getPresentation().setEnabled(false);
         }
+    }
+
+    protected void refreshFailPanel(Project proj, String failInfo) {
+        ToolWindow toolWindow =
+                ToolWindowManager.getInstance(proj).getToolWindow(TuningIDEConstant.HYPER_TUNER_TOOL_WINDOW_ID);
+        TuningConnectFailPanel tuningConnectFailPanel = new TuningConnectFailPanel(toolWindow, proj, failInfo);
+        if (toolWindow != null) {
+            toolWindow.getContentManager().removeAllContents(true);
+            toolWindow.getContentManager().addContent(tuningConnectFailPanel.getContent());
+            toolWindow.getContentManager().setSelectedContent(tuningConnectFailPanel.getContent());
+        }
+    }
+
+    private void synchronizedLeftTree(String failInfo) {
+        // 如果打开多个project， 同步每一个project左侧树状态
+        Project[] openProjects = ProjectUtil.getOpenProjects();
+        ApplicationManager.getApplication().invokeLater(() -> {
+            for (Project proj : openProjects) {
+                // 配置服务器完成后刷新左侧树面板为已配置面板
+                refreshFailPanel(proj, failInfo);
+            }
+        });
     }
 }
